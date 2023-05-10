f2165d3025fd8d7547b804c51456fc3c392355b1
shaswat singh <shaswat.s@humaniticsit.local>
2023-05-10 13:15:31 +0530

    jquery implementation

--- src/app/j-query/j-query.component.ts
+++ src/app/j-query/j-query.component.ts
@@ -10,12 +10,24 @@ export class JQueryComponent {
   toggle = 0;
 
   ngOnInit() {
-    $("#changeSubheading1").on("click", function() {
-      if ($("#toggle") % 2 === 1) $("#subheading1").text("jQuery is a fast, lightweight and feature-rich library of JavaScript.");
+    // on("click", () => {}) | .text()
+    $("#changeSubheading1").on("click", () => {
+      if (this.toggle % 2 === 1) $("#subheading1").text("jQuery is a fast, lightweight and feature-rich library of JavaScript.");
       else $("#subheading1").text("JavaScript (JS) is the most popular lightweight, interpreted compiled programming language.");
-      $("#toggle").val($("#toggle") + 1);
-      console.log($("#toggle"));
+      this.toggle++;
     });
 
+    // .attr("attribute", "value")
+    $("#primaryBtn").on("click", () => $("#alert").attr("class", "alert alert-primary"));
+    $("#secondaryBtn").on("click", () => $("#alert").attr("class", "alert alert-secondary"));
+    $("#successBtn").on("click", () => $("#alert").attr("class", "alert alert-success"));
+    $("#dangerBtn").on("click", () => $("#alert").attr("class", "alert alert-danger"));
+    $("#darkBtn").on("click", () => $("#alert").attr("class", "alert alert-dark"));
+
+    $("#togglePoem").on("click", () => {
+      $("#poemCard").slideToggle();
+      $("#poem").slideToggle();
+      $("#poemImg").slideToggle();
+    })
   }
 }
